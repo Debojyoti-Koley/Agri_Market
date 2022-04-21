49cdfd4b6b537f404121796b594d2a6027f2ab1f
Debojyoti <71626714+Debojyoti-Koley@users.noreply.github.com>
2022-04-21 14:18:41 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,7 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+
+const navBarlinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () =>{
+    navBarlinks.classList.toggle('active');
+});
